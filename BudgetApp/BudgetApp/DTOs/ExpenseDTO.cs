29f2561b0ebee1c3c3c23dd09d389e6b05244f32
Matthew Hile <mattwhile83@gmail.com>
2025-06-18 21:55:09 -0400

    Created expense DTO and service method to get all expense

---
+++ BudgetApp/BudgetApp/DTOs/ExpenseDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace BudgetApp.DTOs
+{
+    public class ExpenseDTO
+    {
+        public int Id { get; set; }
+
+        public string? Description { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public DateOnly Date { get; set; }
+
+        public int? BudgetId { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
---
+++ BudgetApp/BudgetApp/Services/ExpenseService.cs
@@ -0,0 +1,31 @@
+﻿using BudgetApp.Data;
+using BudgetApp.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace BudgetApp.Services
+{
+    public class ExpenseService
+    {
+        private readonly BudgetAppContext _context;
+
+        public ExpenseService(BudgetAppContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<List<ExpenseDTO>> GetAllExpensesAsync()
+        {
+            return await _context.Expenses
+                .Select(e => new ExpenseDTO
+                {
+                    Id = e.Id,
+                    Description = e.Description,
+                    Amount = e.Amount,
+                    Date = e.Date,
+                    BudgetId = e.BudgetId,
+                    UserId = e.UserId
+                })
+                .ToListAsync();
+        }
+    }
+}
