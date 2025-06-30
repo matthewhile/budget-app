24299b5fcdbb21dc801b42174964861259f18ad3
Matthew Hile <mattwhile83@gmail.com>
2025-06-29 22:00:12 -0400

    Added a static form for adding a new budget

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,20 +3,21 @@ import { useState } from 'react';
 import { Container, Stack, Button } from 'react-bootstrap';
 import BudgetCard from './components/BudgetCard';
 import ViewExpensesModal from "./components/ViewExpensesModal";
+import AddBudgetModal from "./components/AddBudgetModal";
 import { useBudgets } from './contexts/BudgetContext';
-
 
 
 function App() {
 
   const { allBudgets } = useBudgets();
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false)
   const [viewExpensesModal, setViewExpensesModal] = useState();
   return (
       <>
           <Container className="my-4">
               <Stack direction="horizontal" gap="2" className="mb-4">
                       <h1 className="me-auto">Budgets</h1>
-                  <Button variant="primary">Add Budget</Button>
+                  <Button variant="primary" onClick={() => setShowAddBudgetModal(true)}>Add Budget</Button>
                   <Button variant="outline-primary">Add Expense</Button>
               </Stack>
               <div className="budgetCards">
@@ -36,10 +37,14 @@ function App() {
                   ))}                 
               </div>
           </Container>
+          <AddBudgetModal
+                show={showAddBudgetModal}
+                handleClose={() => setShowAddBudgetModal(false)}
+          />
           <ViewExpensesModal
                 budgetId={viewExpensesModal}
                 handleClose={() => setViewExpensesModal()}
-            />
+          />
     </>           
   )
 }
