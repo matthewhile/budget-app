3f072d34f7e292318d96caa82ce96764c8e5285e
Matthew Hile <mattwhile83@gmail.com>
2025-12-06 21:45:35 -0500

    Updated login page with a conditional login failed message and password visibility toggle

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,20 +2,30 @@ import '../styles/Login.css';
 import { Form, Button } from "react-bootstrap"
 import { useRef, useState } from "react"
 import {useAuth} from "../contexts/AuthContext"
+import { FaEye, FaEyeSlash } from "react-icons/fa6";
 
 function Login() {
 
     const emailRef = useRef()
     const passwordRef = useRef()
-
     const {login} = useAuth()
+    const [error, setError] = useState("")
+    const [showPassword, setShowPassword] = useState(false)
 
-    function handleSubmit(e) {
+    async function handleSubmit(e) {
         e.preventDefault()
         const email = emailRef.current.value
         const password = passwordRef.current.value 
 
-        login(email, password)
+        const success = await login(email, password)
+
+        if (!success) {
+            setError("Incorrect Username or Password! Please try again.")
+        }
+    }
+
+    function togglePasswordVisibility() {
+        setShowPassword(prev => !prev)
     }
 
     return (
@@ -23,14 +33,19 @@ function Login() {
             <div className="login-box">
                 <h2 className="text-center mb-4">Login</h2>
                 <Form onSubmit={handleSubmit}>
-                    <div className="text-danger"></div>
+                    <div className="text-danger mb-3">{error}</div>
                     <Form.Group className="mb-3">
                         <Form.Label className="login-label">Email</Form.Label>
                         <Form.Control ref={emailRef} type="text" required/>
                     </Form.Group>
                     <Form.Group className="mb-3">
                         <Form.Label className="login-label">Password</Form.Label>
-                        <Form.Control ref={passwordRef} type="password" required/>
+                        <div className="password-wrapper">
+                            <Form.Control ref={passwordRef} id="passwordField" type={showPassword ? "text" : "password"} required></Form.Control>
+                            <span className="password-toggle-icon" onClick={togglePasswordVisibility}>
+                                {showPassword ? (<FaEye size={20} />) : (<FaEyeSlash size={20} />)}
+                            </span>
+                        </div>
                     </Form.Group>
                     <Button className="login-btn" type="submit">Login</Button>
                 </Form>
